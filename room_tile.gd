221e8e0788407f7d0bb0f6e8f6bdb8f8840df2a0
Angela <angelaliuyl@live.com>
2024-04-17 23:52:28 +0100

    changed tile node to CharacterBody2D
    * changed node type to CharacterBody2D to make use of collision logic (move_and_slide)
    * added some child nodes to try to do locking (non-functional atm)

--- room_tile.gd
+++ room_tile.gd
@@ -1,7 +1,9 @@
-extends Area2D
+extends CharacterBody2D
 
 var dragging = false
-var offset = Vector2(0,0)
+var offset = Vector2.ZERO
+var collision = false
+var right_area_entered = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -10,17 +12,33 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	velocity = Vector2.ZERO
 	if dragging:
-		position = get_global_mouse_position() - offset
+		var current_position = position
+		var new_position = get_global_mouse_position() - offset
+		
+		_move_and_slide_alt(current_position, new_position, delta)
+		offset = get_global_mouse_position() - position
+		
+func _move_and_slide_alt(currentPosition, newPosition, delta):
+	var distance = currentPosition.distance_to(newPosition)
+	var direction = currentPosition.direction_to(newPosition)
+	
+	var speed = distance / delta
+	velocity = direction * speed
+	
+	move_and_slide()
 
 func start(pos):
 	position = pos
 	show()
+	$CollisionShape2D.disabled = false
 
 func _on_button_button_down():
 	dragging = true
 	offset = get_global_mouse_position() - global_position
 
-
 func _on_button_button_up():
 	dragging = false
+
+
